8b00eb79966a2ac397baa083c648775bff57f93f
alvarofuentes13 <muchoalvaro@gmail.com>
2025-02-26 22:07:30 +0100

    Intento Api 2.0

--- app/src/main/java/com/example/gustavioandroidstudio/api/ApiClient.java
+++ app/src/main/java/com/example/gustavioandroidstudio/api/ApiClient.java
@@ -6,16 +6,22 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.56.1:8080";
+    //  misma IP de la base de datos, pero el puerto controlado para que sea el del servidor backend
+    public static String BASE_URL_BACKEND = "http://192.168.56.1:8080/";
 
-    private static Retrofit retrofit ;
+    private static Retrofit retrofit;
+
     public static Retrofit getClient(){
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-        if (retrofit==null){
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+
+        if (retrofit == null){
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(BASE_URL_BACKEND)
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(client)
                     .build();
